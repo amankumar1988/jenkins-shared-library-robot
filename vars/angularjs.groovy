c6a66342057f03e1aff5860659be49aa1f2ffdfb
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-07 17:58:49 +0530

    Auto Push

---
+++ vars/angularjs.groovy
@@ -0,0 +1,23 @@
+def lintChecks(COMPONENT){
+    sh '''
+        echo Lint Checks for ${COMPONENT}       
+        echo Performing lint checks for ${COMPONENT}
+        echo Performing lint checks completed ${COMPONENT}
+    '''
+}
+// call is the default function which will be called when you call the filename
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Lint Checks') {
+                steps{
+                    script {
+                        lintChecks()
+                    }
+                }  
+             }
+        }
+    }
+}
