50790ff93cef13233f33bf2d849ced14ca87c1f8
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-09 15:51:08 +0530

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -89,7 +89,7 @@ def call() {
                 }
                 steps{
                     sh "echo Publishing the artifacts"
-                    sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"  
+                    sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"  
                 }
             }
 
