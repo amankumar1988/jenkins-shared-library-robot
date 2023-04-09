e2ecc1c305c347ce0571d0de82b118d098840a61
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-09 15:14:42 +0530

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,7 @@
 def sonarChecks(){
-    sh '''
-        // sonar-scanner -Dsonar.host.url=http://172.31.1.163:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
-        // curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
-        // bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}
-        echo "Starting the Code Quality Analysis"
-        echo "Code Quality Analysis Completed"
-    '''
+    // sh  "sonar-scanner -Dsonar.host.url=http://172.31.1.163:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+    // sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+    // sh "bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
+    sh "echo Starting the Code Quality Analysis"
+    sh echo Code Quality Analysis Completed"
 }
