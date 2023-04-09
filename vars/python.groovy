e135219955c0579719fcc12ed5e26564ce1e9ffb
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-09 14:22:17 +0530

    Auto Push

--- vars/python.groovy
+++ vars/python.groovy
@@ -26,6 +26,7 @@ def call() {
             stage('Sonar Checks') {
                 steps{
                     script {
+                        env.ARGS ="-Dsonar.sources=."
                         common.sonarChecks()
                     }
                 }  
