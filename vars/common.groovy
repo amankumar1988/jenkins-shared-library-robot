4e316cc3b3c91adc8b7b44e6a5190df0a57da522
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-10 22:12:58 +0530

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -109,5 +109,9 @@ def artifacts(){
             '''
             }
         }
+        stage('Upload the artifacts'){
+            withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+            sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+        }
     }
 }
